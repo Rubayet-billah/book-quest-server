4858b6512ac921e8e065917dd5490ef9f13bce99
Rubayet-billah <rubayetbillah909@gmail.com>
2023-07-16 14:59:49 +0600

    apierror added

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -3,6 +3,7 @@
 /* eslint-disable no-unused-expressions */
 import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
 import { IGenericErrorMessage } from "../../interfaces/error";
+import ApiError from "../../errors/ApiError";
 
 const globalErrorHandler: ErrorRequestHandler = (
   error,
@@ -10,11 +11,26 @@ const globalErrorHandler: ErrorRequestHandler = (
   res: Response,
   next: NextFunction
 ) => {
+  // process.env.NODE_ENV === "development"
+  //   ? console.log(`🐱‍🏍 globalErrorHandler ~~`, { error })
+  //   : errorlogger.error(`🐱‍🏍 globalErrorHandler ~~`, error);
+
   let statusCode = 500;
   let message = "Something went wrong !";
   let errorMessages: IGenericErrorMessage[] = [];
   if (error instanceof Error) {
     message = error?.message;
+    errorMessages = error?.message
+      ? [
+          {
+            path: "",
+            message: error?.message,
+          },
+        ]
+      : [];
+  } else if (error instanceof ApiError) {
+    statusCode = error?.statusCode;
+    message = error.message;
     errorMessages = error?.message
       ? [
           {
